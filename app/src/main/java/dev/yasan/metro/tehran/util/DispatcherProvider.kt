f271211ba23fde1e1b45cedf615d51ef19efc909
YASAN <yasandev@gmail.com>
2021-12-19 01:31:55 +0330

    Create DispatcherProvider.kt

---
+++ app/src/main/java/dev/yasan/metro/tehran/util/DispatcherProvider.kt
@@ -0,0 +1,10 @@
+package dev.yasan.metro.tehran.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+interface DispatcherProvider {
+    val main: CoroutineDispatcher
+    val io: CoroutineDispatcher
+    val default: CoroutineDispatcher
+    val unconfined: CoroutineDispatcher
+}
