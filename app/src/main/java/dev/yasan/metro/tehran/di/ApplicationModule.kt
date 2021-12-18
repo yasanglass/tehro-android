ec29b2721a53b1411130d82821615a3e43705f96
YASAN <yasandev@gmail.com>
2021-12-19 01:44:18 +0330

    Create ApplicationModule.kt

---
+++ app/src/main/java/dev/yasan/metro/tehran/di/ApplicationModule.kt
@@ -0,0 +1,29 @@
+package dev.yasan.metro.tehran.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dev.yasan.metro.tehran.util.DispatcherProvider
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule {
+
+    @Singleton
+    @Provides
+    fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider {
+        override val main: CoroutineDispatcher
+            get() = Dispatchers.Main
+        override val io: CoroutineDispatcher
+            get() = Dispatchers.IO
+        override val default: CoroutineDispatcher
+            get() = Dispatchers.Default
+        override val unconfined: CoroutineDispatcher
+            get() = Dispatchers.Unconfined
+    }
+
+}
