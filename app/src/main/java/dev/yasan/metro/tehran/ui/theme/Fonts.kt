4138ebdb96e7e65986b4c436ade6fde22df1b355
YASAN <yasandev@gmail.com>
2021-12-19 05:07:29 +0330

    Create Fonts.kt

---
+++ app/src/main/java/dev/yasan/metro/tehran/ui/theme/Fonts.kt
@@ -0,0 +1,33 @@
+package dev.yasan.metro.tehran.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import dev.yasan.metro.tehran.R
+
+val rubikFamily = FontFamily(
+    Font(R.font.rubik_black, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.rubik_blackitalic, FontWeight.Black, FontStyle.Italic),
+
+    Font(R.font.rubik_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.rubik_bolditalic, FontWeight.Bold, FontStyle.Italic),
+
+    Font(R.font.rubik_light, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.rubik_lightitalic, FontWeight.Light, FontStyle.Italic),
+
+    Font(R.font.rubik_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.rubik_mediumitalic, FontWeight.Medium, FontStyle.Italic),
+
+    Font(R.font.rubik_regular, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.rubik_italic, FontWeight.Normal, FontStyle.Italic),
+)
+
+val vazirFamily = FontFamily(
+    Font(R.font.vazir_black_ui, FontWeight.Black, FontStyle.Normal),
+    Font(R.font.vazir_bold_ui, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.vazir_light_ui, FontWeight.Light, FontStyle.Normal),
+    Font(R.font.vazir_medium_ui, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.vazir_regular_ui, FontWeight.Normal, FontStyle.Normal),
+    Font(R.font.vazir_thin_ui, FontWeight.Thin, FontStyle.Normal),
+)
