b452ee3b2475c6e7decc1fe2553726c5ad1324f2
YASAN <yasandev@gmail.com>
2021-12-19 05:24:20 +0330

    Create LocaleHelper.properFontFamily()

--- app/src/main/java/dev/yasan/metro/tehran/util/LocaleHelper.kt
+++ app/src/main/java/dev/yasan/metro/tehran/util/LocaleHelper.kt
@@ -1,5 +1,8 @@
 package dev.yasan.metro.tehran.util
 
+import androidx.compose.ui.text.font.FontFamily
+import dev.yasan.metro.tehran.ui.theme.rubikFamily
+import dev.yasan.metro.tehran.ui.theme.vazirFamily
 import java.util.*
 
 object LocaleHelper {
@@ -8,5 +11,10 @@ object LocaleHelper {
      * Checks if the default device language is Farsi/Persian.
      */
     val isFarsi get() = Locale.getDefault().language == "fa"
+
+    /**
+     * Returns the proper font family based on the app language.
+     */
+    val properFontFamily get(): FontFamily = if (isFarsi) vazirFamily else rubikFamily
 
 }
