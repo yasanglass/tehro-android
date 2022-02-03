6b2e45d90bbf9a689cb677f5c5699ae9656d5816
YASAN <yasandev@gmail.com>
2022-02-04 01:03:42 +0330

    Create TehDivider.kt

---
+++ app/src/main/java/dev/yasan/metro/tehran/ui/composable/common/teh/TehDivider.kt
@@ -0,0 +1,16 @@
+package dev.yasan.metro.tehran.ui.composable.common.teh
+
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import dev.yasan.metro.tehran.R
+import dev.yasan.metro.tehran.ui.theme.dimenDivider
+
+/**
+ * Basic Tehro-themed divider. Should be used instead of the generic divider for easy theming.
+ */
+@Composable
+fun TehDivider(modifier: Modifier = Modifier) {
+    Divider(color = colorResource(id = R.color.divider), thickness = dimenDivider)
+}
