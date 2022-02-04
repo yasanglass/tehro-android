3606a426b05acdb1c100a6b03cc4675a7c6c2063
YASAN <yasandev@gmail.com>
2022-02-04 03:33:06 +0330

    Create StationAccessibilityDAO.kt

---
+++ app/src/main/java/dev/yasan/metro/tehran/data/db/dao/StationAccessibilityDAO.kt
@@ -0,0 +1,28 @@
+package dev.yasan.metro.tehran.data.db.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import dev.yasan.metro.tehran.data.db.MetroDatabase
+import dev.yasan.metro.tehran.data.db.entity.StationAccessibility
+
+/**
+ * Data access object for [StationAccessibility].
+ *
+ * @see [MetroDatabase]
+ */
+@Dao
+interface StationAccessibilityDAO {
+
+    /**
+     * @return A list of all [StationAccessibility]s in [MetroDatabase].
+     */
+    @Query("SELECT * FROM stations_accessibility")
+    suspend fun getAll(): List<StationAccessibility>
+
+    /**
+     * @return A [StationAccessibility] with the matching station id if any exist.
+     */
+    @Query("SELECT * FROM stations_accessibility WHERE station_id=:stationId")
+    suspend fun getByStationId(stationId: Int): StationAccessibility?
+
+}
