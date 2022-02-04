368a3e5f08e018bff7d6fb635964c620b964717d
YASAN <yasandev@gmail.com>
2022-02-04 03:30:47 +0330

    Update StationAccessibility.kt

--- app/src/main/java/dev/yasan/metro/tehran/data/db/entity/StationAccessibility.kt
+++ app/src/main/java/dev/yasan/metro/tehran/data/db/entity/StationAccessibility.kt
@@ -24,7 +24,36 @@ import androidx.room.PrimaryKey
 data class StationAccessibility(
     @PrimaryKey(autoGenerate = false)
     @ColumnInfo(name = "station_id") val stationId: Int,
-    @ColumnInfo(name = "emergency_medical_services") val emergencyMedicalServices: Int,
-    @ColumnInfo(name = "wheelchair_accessibility_level") val wheelchairAccessibilityLevel: Int,
-    @ColumnInfo(name = "blind_accessibility_level") val blindAccessibilityLevel: Int,
-)
+    @ColumnInfo(name = "emergency_medical_services") val emergencyMedicalServicesInt: Int,
+    @ColumnInfo(name = "wheelchair_accessibility_level") val wheelchairAccessibilityLevelInt: Int,
+    @ColumnInfo(name = "blind_accessibility_level") val blindAccessibilityLevelInt: Int,
+) {
+
+    /**
+     * If the station has emergency medical services.
+     */
+    val emergencyMedicalServices get() = emergencyMedicalServicesInt == 1
+
+    val wheelchairAccessibilityLevel get() = AccessibilityWheelchair.fromInt(wheelchairAccessibilityLevelInt)
+
+    val blindAccessibilityLevel get() = AccessibilityBlind.fromInt(blindAccessibilityLevelInt)
+
+}
+
+enum class AccessibilityWheelchair {
+    NONE, LEVEL_1, LEVEL_2, LEVEL_3, MAX, INVALID;
+
+    companion object {
+        fun fromInt(value: Int) = values().getOrNull(value) ?: INVALID
+    }
+
+}
+
+enum class AccessibilityBlind {
+    NONE, PLATFORMS_ONLY, FULL, INVALID;
+
+    companion object {
+        fun fromInt(value: Int) = AccessibilityBlind.values().getOrNull(value) ?: AccessibilityBlind.INVALID
+    }
+
+}
