a81cede7500128da7c52713c8bb4371fa308729f
YASAN <yasandev@gmail.com>
2022-03-30 00:31:46 +0430

    Add Firebase

--- app/src/main/java/dev/yasan/metro/tehran/ApplicationClass.kt
+++ app/src/main/java/dev/yasan/metro/tehran/ApplicationClass.kt
@@ -1,10 +1,26 @@
 package dev.yasan.metro.tehran
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.perf.FirebasePerformance
 import dagger.hilt.android.HiltAndroidApp
 
 /**
  * The application class of Tehro.
  */
 @HiltAndroidApp
-class ApplicationClass : Application()
+class ApplicationClass : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Disable Firebase on debug builds
+        val firebaseEnabled = !BuildConfig.DEBUG
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(firebaseEnabled)
+        FirebasePerformance.getInstance().isPerformanceCollectionEnabled = firebaseEnabled
+        FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(!firebaseEnabled)
+    }
+
+
+}
