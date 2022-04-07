75c64cfeeb62ea1ec6648834c165c08a81aebd8e
YASAN <yasandev@gmail.com>
2022-04-07 17:52:16 +0430

    Sort stations in line based no position_in_line

--- app/src/main/java/dev/yasan/metro/tehran/data/db/dao/StationDAO.kt
+++ app/src/main/java/dev/yasan/metro/tehran/data/db/dao/StationDAO.kt
@@ -28,7 +28,7 @@ interface StationDAO {
     /**
      * @return A [Station] with matching line id if any exist.
      */
-    @Query("SELECT * FROM stations WHERE line_id=:lineId")
+    @Query("SELECT * FROM stations WHERE line_id=:lineId ORDER BY position_in_line")
     suspend fun getByLineId(lineId: Int): List<Station>
 
     /**
