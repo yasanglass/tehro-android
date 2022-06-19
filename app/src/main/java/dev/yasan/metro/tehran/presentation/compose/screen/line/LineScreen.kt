cf40183059846c1c60bb84a5d86a37089eb67467
Yasan <yasandev@gmail.com>
2022-06-19 23:24:51 +0430

    Pass station distance to StationItem on LineScreen

--- app/src/main/java/dev/yasan/metro/tehran/presentation/compose/screen/line/LineScreen.kt
+++ app/src/main/java/dev/yasan/metro/tehran/presentation/compose/screen/line/LineScreen.kt
@@ -1,22 +1,24 @@
 package dev.yasan.metro.tehran.presentation.compose.screen.line
 
+import android.location.Location
+import android.util.Log
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.sharp.SwapVert
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.birjuvachhani.locus.Locus
+import com.birjuvachhani.locus.LocusResult
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dev.yasan.kit.compose.foundation.grid
 import dev.yasan.kit.core.Resource
@@ -49,6 +51,8 @@ fun LineScreen(
     lineId: Int,
 ) {
 
+    val context = LocalContext.current
+
     val title = lineViewModel.title.observeAsState(initial = "")
     val lineColor = lineViewModel.lineColor.observeAsState(initial = Color.DarkGray)
     val stations = lineViewModel.stations.observeAsState(initial = Resource.Initial())
@@ -64,6 +68,9 @@ fun LineScreen(
     }
 
     val buttonAngle by animateFloatAsState(if (orderAscending.value) 180f else 0f)
+
+    val locusResult: State<LocusResult?> = Locus.startLocationUpdates(context).observeAsState()
+    val userLocation = locusResult.value?.location
 
     TehScreen(
         title = title.value,
@@ -108,11 +115,19 @@ fun LineScreen(
                     },
                     key = { it.id }
                 ) { station ->
+                    val distance = if (userLocation == null || !station.hasLocation) null else {
+                        val stationLocation = Location("")
+                        stationLocation.latitude = station.locationLatitude ?: 0.0
+                        stationLocation.longitude = station.locationLongitude ?: 0.0
+                        val distance = stationLocation.distanceTo(userLocation)
+                        distance.toInt()
+                    }
                     StationItem(
                         modifier = Modifier.animateItemPlacement(),
                         station = station,
                         navController = navController,
-                        launchSource = LaunchSource.LINE
+                        launchSource = LaunchSource.LINE,
+                        distance = distance
                     )
                 }
 
@@ -126,5 +141,13 @@ fun LineScreen(
                 }
             }
         }
+
     }
+
+    DisposableEffect(key1 = lineViewModel) {
+        onDispose {
+            Locus.stopLocationUpdates()
+        }
+    }
+
 }
